aca8bbd986fecc3989f7b42d268b9eda85f7af06
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-02-09 13:17:48 +0000

    add profile view skelton

---
+++ lib/Features/ProfileScreen/Presentation/ProfileView.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+import 'Widgets/ProfileViewBody.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProfileViewBody();
+  }
+}
---
+++ lib/Features/ProfileScreen/Presentation/Widgets/ProfileViewBody.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class ProfileViewBody extends StatelessWidget {
+  const ProfileViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
