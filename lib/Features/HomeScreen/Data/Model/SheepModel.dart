c5ba766399298677256bd0d8d3a7367e75b9b5fd
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-02-16 14:03:18 +0000

    create sheep model

---
+++ lib/Features/HomeScreen/Data/Model/SheepModel.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: file_names
+
+class SheepModel {
+  final String id;
+  final String state;
+  final String sexe;
+  final int weight;
+  final int age;
+  final String lastBirth;
+  final int children;
+  final String lastVisit;
+
+  SheepModel({
+    required this.id,
+    required this.state,
+    required this.sexe,
+    required this.weight,
+    required this.age,
+    required this.lastBirth,
+    required this.children,
+    required this.lastVisit,
+  });
+}
