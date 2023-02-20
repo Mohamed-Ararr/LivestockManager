1aa30954945cfb099ea69394ddc01a11061cee06
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-02-20 15:16:11 +0000

    refactor code + add sold / available button

--- lib/Core/Utils/soldOption.dart
+++ lib/Core/Utils/soldOption.dart
@@ -3,9 +3,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:sheepmanager/Features/HomeScreen/Data/Model/sheep_model.dart';
 
 class SoldOption extends StatelessWidget {
-  const SoldOption({super.key});
+  const SoldOption({super.key, required this.sheep});
+
+  final SheepModel sheep;
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +20,14 @@ class SoldOption extends StatelessWidget {
           bottomRight: Radius.circular(10),
         ),
         child: IconSlideAction(
-          onTap: () {},
+          onTap: () {
+            sheep.state == "Sold"
+                ? sheep.state = "Available"
+                : sheep.state = "Sold";
+            sheep.save();
+          },
           color: Colors.green,
-          caption: "Sold",
+          caption: sheep.state == "Sold" ? "Available" : "Sold",
           iconWidget: const Padding(
             padding: EdgeInsets.only(bottom: 10),
             child: Icon(
