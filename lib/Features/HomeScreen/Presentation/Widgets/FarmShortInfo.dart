031eadfb031ac68efcc6807faf1fb43c0de55bca
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-03-27 15:57:10 +0000

    add farmshortinfo box

---
+++ lib/Features/HomeScreen/Presentation/Widgets/FarmShortInfo.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../../../../Core/Utils/colors.dart';
+import 'FarmBoxAttribute.dart';
+
+class FarmShortInfo extends StatelessWidget {
+  const FarmShortInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(15),
+          topRight: Radius.circular(15),
+        ),
+        color: AppColors.greyColor,
+      ),
+      // margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          FarmBoxAttribute(
+            title: "Farm ID",
+            input: "1024648",
+          ),
+          SizedBox(height: 15),
+          FarmBoxAttribute(
+            title: "Farm Owner",
+            input: "Mohamed arar",
+          ),
+          SizedBox(height: 15),
+          FarmBoxAttribute(
+            title: "Farm Address",
+            input: "Regai Bachir",
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/Features/HomeScreen/Presentation/Widgets/HomeViewBody.dart
+++ lib/Features/HomeScreen/Presentation/Widgets/HomeViewBody.dart
@@ -7,6 +7,7 @@ import 'package:sheepmanager/Core/Utils/colors.dart';
 import 'package:sheepmanager/Core/Utils/fonts.dart';
 import 'package:sheepmanager/Core/Utils/router.dart';
 import 'package:sheepmanager/Features/HomeScreen/Presentation/Widgets/FarmBoxAttribute.dart';
+import 'package:sheepmanager/Features/HomeScreen/Presentation/Widgets/FarmShortInfo.dart';
 
 import '../../../../constValues.dart';
 import 'CustomAppBar.dart';
@@ -49,39 +50,9 @@ class _HomeViewBodyState extends State<HomeViewBody> {
                   return Container(
                     margin: const EdgeInsets.symmetric(vertical: 10),
                     child: Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(15),
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(15),
-                              topRight: Radius.circular(15),
-                            ),
-                            color: AppColors.greyColor,
-                          ),
-                          // margin: const EdgeInsets.symmetric(vertical: 10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              FarmBoxAttribute(
-                                title: "Farm ID",
-                                input: "1024648",
-                              ),
-                              SizedBox(height: 15),
-                              FarmBoxAttribute(
-                                title: "Farm Owner",
-                                input: "Mohamed arar",
-                              ),
-                              SizedBox(height: 15),
-                              FarmBoxAttribute(
-                                title: "Farm Address",
-                                input: "Regai Bachir",
-                              ),
-                            ],
-                          ),
-                        ),
-                        const TapMoreButton()
+                      children: const [
+                        FarmShortInfo(),
+                        TapMoreButton(),
                       ],
                     ),
                   );
