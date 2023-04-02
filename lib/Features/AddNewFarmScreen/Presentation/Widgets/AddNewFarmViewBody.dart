6787de756fb3333aa85386fa18443348e5f1eab6
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-04-02 14:36:31 +0000

    add route to addnewfarmview

--- lib/Features/AddNewFarmScreen/Presentation/Widgets/AddNewFarmViewBody.dart
+++ lib/Features/AddNewFarmScreen/Presentation/Widgets/AddNewFarmViewBody.dart
@@ -1,12 +1,27 @@
 // ignore_for_file: file_names
 
 import "package:flutter/material.dart";
+import "package:sheepmanager/Features/HomeScreen/Presentation/Widgets/CustomAppBar.dart";
+import "package:sheepmanager/constValues.dart";
 
 class AddNewFarmViewBody extends StatelessWidget {
   const AddNewFarmViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: kPaddingRightLeft,
+            child: Column(
+              children: [
+                CustomAppBar(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
