5ecc00a1f51d2f69e7e5eb319f9815525870ecf0
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-04-02 14:33:35 +0000

    add addnewfarm Screen widgets

---
+++ lib/Features/AddNewFarmScreen/Presentation/Widgets/AddNewFarmViewBody.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import "package:flutter/material.dart";
+
+class AddNewFarmViewBody extends StatelessWidget {
+  const AddNewFarmViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
